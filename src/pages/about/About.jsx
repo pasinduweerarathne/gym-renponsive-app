59b22d99c0cd6970d4aa661cb66fb78924924a13
H.K.D.Pasindu Pramod Weerarathne <pasiweerarathne98@gmail.com>
2022-09-16 23:21:22 +0530

    add aboutus page

--- src/pages/about/About.jsx
+++ src/pages/about/About.jsx
@@ -1,7 +1,91 @@
+import Header from "../../components/Header";
+import HeaderImg from "../../images/header_bg_1.jpg";
+import StoryImg from "../../images/about1.jpg";
+import VisionImg from "../../images/about2.jpg";
+import MissionImg from "../../images/about3.jpg";
 import "./About.css";
 
 const About = () => {
-  return <div>About</div>;
+  return (
+    <>
+      <Header title="About Us" image={HeaderImg}>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptatibus
+        eveniet cupiditate nemo optio officiis deserunt facilis fuga assumenda
+        itaque sunt.
+      </Header>
+
+      <section className="about_story">
+        <div className="container about_story-container">
+          <div className="about_section-img">
+            <img src={StoryImg} alt="" />
+          </div>
+          <div className="about_section-content">
+            <h1>Our Story</h1>
+            <p>
+              Lorem ipsum dolor sit amet consectetur, adipisicing elit. Expedita
+              atque eum ex ipsam maiores nostrum animi magnam laudantium, est
+              distinctio sint quis nulla, sapiente repellendus obcaecati et
+            </p>
+            <p>
+              Lorem ipsum dolor sit amet consectetur, adipisicing elit. Expedita
+              atque eum ex ipsam maiores nostrum animi magnam laudantium, est
+              distinctio sint quis nulla, sapiente repellendus obcaecati et
+            </p>
+            <p>
+              Lorem ipsum dolor sit amet consectetur, adipisicing elit. Expedita
+              atque eum ex ipsam maiores nostrum animi magnam laudantium,
+            </p>
+          </div>
+        </div>
+      </section>
+
+      <section className="about_vision">
+        <div className="container about_vision-container">
+          <div className="about_section-content">
+            <h1>Our Vision</h1>
+            <p>
+              Lorem ipsum dolor sit amet consectetur, adipisicing elit. Expedita
+              atque eum ex ipsam maiores nostrum animi magnam laudantium, est
+              distinctio sint quis nulla, sapiente repellendus obcaecati et
+            </p>
+            <p>
+              Lorem ipsum dolor sit amet consectetur, adipisicing elit. Expedita
+              atque eum ex ipsam maiores nostrum animi magnam laudantium, est
+              distinctio sint quis nulla, sapiente repellendus obcaecati et
+            </p>
+          </div>
+          <div className="about_section-img">
+            <img src={VisionImg} alt="" />
+          </div>
+        </div>
+      </section>
+
+      <section className="about_mission">
+        <div className="container about_mission-container">
+          <div className="about_section-img">
+            <img src={MissionImg} alt="" />
+          </div>
+          <div className="about_section-content">
+            <h1>Our Mission</h1>
+            <p>
+              Lorem ipsum dolor sit amet consectetur, adipisicing elit. Expedita
+              atque eum ex ipsam maiores nostrum animi magnam laudantium, est
+              distinctio sint quis nulla, sapiente repellendus obcaecati et
+            </p>
+            <p>
+              Lorem ipsum dolor sit amet consectetur, adipisicing elit. Expedita
+              atque eum ex ipsam maiores nostrum animi magnam laudantium, est
+              distinctio sint quis nulla, sapiente repellendus obcaecati et
+            </p>
+            <p>
+              Lorem ipsum dolor sit amet consectetur, adipisicing elit. Expedita
+              atque eum ex ipsam maiores nostrum animi magnam laudantium,
+            </p>
+          </div>
+        </div>
+      </section>
+    </>
+  );
 };
 
 export default About;
--- src/pages/contact/Contact.jsx
+++ src/pages/contact/Contact.jsx
@@ -1,7 +1,34 @@
+import { IoLogoWhatsapp } from "react-icons/io";
+import { MdEmail } from "react-icons/md";
+import { BsMessenger } from "react-icons/bs";
+import Header from "../../components/Header";
+import HeaderImg from "../../images/header_bg_2.jpg";
 import "./Contact.css";
 
 const Contact = () => {
-  return <div>Contact</div>;
+  return (
+    <>
+      <Header title="Get In Touch" image={HeaderImg}>
+        Lorem ipsum dolor, sit amet consectetur adipisicing elit. Atque fuga
+        adipisicing elit. Atque fuga eiusad ipisicing elit. Atque fuga
+      </Header>
+      <section className="contact">
+        <div className="container contact_container">
+          <div className="contact_wrapper">
+            <a href="#">
+              <MdEmail />
+            </a>
+            <a href="#">
+              <BsMessenger />
+            </a>
+            <a href="https://wa.me/+94766197751">
+              <IoLogoWhatsapp />
+            </a>
+          </div>
+        </div>
+      </section>
+    </>
+  );
 };
 
 export default Contact;
