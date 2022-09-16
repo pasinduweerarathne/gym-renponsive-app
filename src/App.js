d26361e80d830144d942247a73a9804bfa98764b
H.K.D.Pasindu Pramod Weerarathne <pasiweerarathne98@gmail.com>
2022-09-16 11:47:47 +0530

    add navbar

--- src/App.js
+++ src/App.js
@@ -1,7 +1,26 @@
 import "./App.css";
+import Navbar from "./components/Navbar";
+import About from "./pages/about/About";
+import Contact from "./pages/contact/Contact";
+import Gallery from "./pages/gallery/Gallery";
+import Home from "./pages/home/Home";
+import NotFound from "./pages/notFound/NotFound";
+import Plans from "./pages/plans/Plans";
+import Trainers from "./pages/trainers/Trainers";
 
 function App() {
-  return <>hello</>;
+  return (
+    <>
+      <Navbar />
+      <Home />
+      <About />
+      <Contact />
+      <Gallery />
+      <Plans />
+      <Trainers />
+      <NotFound />
+    </>
+  );
 }
 
 export default App;
---
+++ src/components/Navbar.jsx
@@ -0,0 +1,44 @@
+import "./Navbar.css";
+import Logo from "../images/logo.png";
+import { GoThreeBars } from "react-icons/go";
+import { MdOutlineClose } from "react-icons/md";
+
+import { Link, NavLink } from "react-router-dom";
+import { links } from "../data";
+import { useState } from "react";
+
+const Navbar = () => {
+  const [isNavOpen, setIsNavOpen] = useState(false);
+
+  return (
+    <nav>
+      <div className="container nav_container">
+        <Link to="/">
+          <img className="logo" src={Logo} alt="" />
+        </Link>
+        <ul className={`nav_links ${isNavOpen ? "show_nav" : "hide_nav"}`}>
+          {links.map(({ name, path }, index) => {
+            return (
+              <li key={index}>
+                <NavLink
+                  className={({ isActive }) => (isActive ? "active-nav" : "")}
+                  to={path}
+                >
+                  {name}
+                </NavLink>
+              </li>
+            );
+          })}
+        </ul>
+        <button
+          className="nav_toggle-btn"
+          onClick={() => setIsNavOpen(!isNavOpen)}
+        >
+          {isNavOpen ? <MdOutlineClose /> : <GoThreeBars />}
+        </button>
+      </div>
+    </nav>
+  );
+};
+
+export default Navbar;
