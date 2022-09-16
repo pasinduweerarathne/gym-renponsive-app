0e16bbaa07ed6017477ad02b165a3b902de972f9
H.K.D.Pasindu Pramod Weerarathne <pasiweerarathne98@gmail.com>
2022-09-16 23:20:59 +0530

    add about us page

--- src/pages/gallery/Gallery.jsx
+++ src/pages/gallery/Gallery.jsx
@@ -1,7 +1,15 @@
+import Header from "../../components/Header";
+import HeaderImg from "../../images/header_bg_3.jpg";
 import "./Gallery.css";
 
 const Gallery = () => {
-  return <div>Gallery</div>;
+  return (
+    <>
+      <Header title="Our Gallery" image={HeaderImg}>
+        lorem20
+      </Header>
+    </>
+  );
 };
 
 export default Gallery;
